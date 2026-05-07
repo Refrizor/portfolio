d04a50003cefaf26cb0de4bc21c9bb6771311d66
Refrizor <retizao.tv@gmail.com>
2026-05-07 19:14:00 +0000

    ux: redesign footer; use icons

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,32 @@
-function Footer (){
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {faMailForward} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+const footerLinks = [
+    {name: "GitHub", icon: faGithub, link: "https://github.com/Refrizor"},
+    {name: "LinkedIn", icon: faLinkedin, link: "https://www.linkedin.com/in/dev-collins"},
+    {name: "Email", icon: faMailForward, link: "mailto:devcollins36@gmail.com"},
+];
+
+function Footer() {
     return (
-        <footer className="footer">
+        <footer className="site-footer">
+            <div className="site-footer__grid">
+                <ul>
+                    {footerLinks.map((link) => (
+                        <li key={link.name}>
+                            <span className="site-footer__item">
+                                <a href={link.link}>
+                                    <span className={"site-footer__item__icon"}>
+                                        <FontAwesomeIcon icon={link.icon}/>
+                                    </span>
+                                    <span className={"site-footer__item__label"}>{link.name}</span>
+                                </a>
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </footer>
     )
 }
