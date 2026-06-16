be55099bb28fdb12de7b490819fe7f766dbe8337
Azzy <45441561+Refrizor@users.noreply.github.com>
2026-06-16 18:25:46 -0400

    chore(frameworks/libs): add new libraries
    Added zod, redux, jest, junit, numpy, and bootstrap

--- frontend/src/features/home/Frameworks.tsx
+++ frontend/src/features/home/Frameworks.tsx
@@ -16,6 +16,34 @@ const frameworks = [
     {
         name: "Flask",
         badge: "https://img.shields.io/badge/Flask-374151?style=flat-square&logo=flask&logoColor=E5E7EB"
+    },
+    {
+        name: "zod",
+        badge: "https://img.shields.io/badge/zod-374151?style=flat-square&logo=zod&logoColor=408AFF"
+    },
+    {
+        name: "Redux",
+        badge: "https://img.shields.io/badge/Redux-374151?style=flat-square&logo=redux&logoColor=764ABC"
+    },
+    {
+        name: "Jest",
+        badge: "https://img.shields.io/badge/Jest-374151?style=flat-square&logo=jest&logoColor=C21325"
+    },
+    {
+        name: "JUnit 5",
+        badge: "https://img.shields.io/badge/JUnit5-374151?style=flat-square&logo=junit5&logoColor=25A162"
+    },
+    {
+        name: "NumPy",
+        badge: "https://img.shields.io/badge/NumPy-374151?style=flat-square&logo=numpy&logoColor=013243"
+    },
+    {
+        name: "pandas",
+        badge: "https://img.shields.io/badge/pandas-374151?style=flat-square&logo=pandas&logoColor=150458"
+    },
+    {
+        name: "Bootstrap",
+        badge: "https://img.shields.io/badge/Bootstrap-374151?style=flat-square&logo=bootstrap&logoColor=7952B3"
     }
 ];
 
